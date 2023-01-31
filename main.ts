14028171b81a1e75060faa0d55feee5c5863f57c
Noah062810 <122543656+Noah062810@users.noreply.github.com>
2023-01-31 12:56:34 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    Player1 += 1
+    Rounds += 1
+    OLED.init(128, 64)
+    updatescores()
+})
+input.onButtonPressed(Button.AB, function () {
+    Ties += 1
+    Rounds += 1
+    OLED.init(128, 64)
+    updatescores()
+})
 function updatescores () {
     OLED.clear()
     OLED.writeStringNewLine("Player1: " + Player1)
@@ -8,15 +20,27 @@ function updatescores () {
     OLED.newLine()
     OLED.writeStringNewLine("Rounds: " + Rounds)
 }
-let Rounds = 0
-let Ties = 0
+input.onButtonPressed(Button.B, function () {
+    Player2 += 1
+    Rounds += 1
+    OLED.init(128, 64)
+    updatescores()
+})
+input.onGesture(Gesture.Shake, function () {
+    reset()
+})
+function reset () {
+    OLED.init(128, 64)
+    OLED.writeStringNewLine("Shall we play a game?")
+    Player1 = 0
+    Player2 = 0
+    Ties = 0
+    Rounds = 0
+    basic.pause(2000)
+    updatescores()
+}
 let Player2 = 0
+let Ties = 0
+let Rounds = 0
 let Player1 = 0
-OLED.init(128, 64)
-OLED.writeStringNewLine("Shall we play a game?")
-Player1 = 0
-Player2 = 0
-Ties = 0
-Rounds = 0
-basic.pause(2000)
-updatescores()
+reset()
